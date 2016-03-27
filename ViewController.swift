3ca39bef3b404147234ac9dfed5e579838242619
jhad7010 <jhad7010@gmail.com>
2016-03-27 09:53:31 -0600

    Version original
    Version original del código

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGit3
+//
+//  Created by Jaime Haddad on 3/27/16.
+//  Copyright © 2016 Jaime Haddad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
